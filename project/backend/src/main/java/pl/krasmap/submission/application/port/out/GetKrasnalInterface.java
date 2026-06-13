9c92425c34d5b7513ef2398410d845b2a2d658b1
N3cro0o <calko.n3cro0o@gmail.com>
2026-06-13 22:54:52 +0200

    Added *forgotten* Submission - Krasnal connection when accepting sub

---
+++ project/backend/src/main/java/pl/krasmap/submission/application/port/out/GetKrasnalInterface.java
@@ -0,0 +1,7 @@
+package pl.krasmap.submission.application.port.out;
+
+import pl.krasmap.submission.application.domain.ReviewKrasnal;
+
+public interface GetKrasnalInterface {
+    void AddNewKrasnal(ReviewKrasnal krasnal);
+}
